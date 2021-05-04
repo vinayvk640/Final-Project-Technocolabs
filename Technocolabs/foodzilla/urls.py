dd46a4262fd5fab7f63559c6ddbd17d2a8906a38
shaikmujahed <shaikmujahed001@gmail.com>
2021-05-04 22:02:21 +0530

    This commit contains back-end functionality.

---
+++ Technocolabs/foodzilla/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeViwe.as_view(), name='home'),
+    #path('contact/', views.contact, name='contact'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    #path('accounts/', views.accounts, name='accounts'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('logout/', views.logoutView.as_view(), name='logoutUser'),
+    
+
+]
+
